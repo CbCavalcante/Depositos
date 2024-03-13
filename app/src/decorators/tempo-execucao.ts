344635f3e0822d0f83b39a4c9b329e63bb8c10f0
Cauã Bernardo <122488009+CbCavalcante@users.noreply.github.com>
2024-03-13 11:33:58 -0400

    Delete app directory

--- app/src/decorators/tempo-execucao.ts
+++
@@ -1,9 +0,0 @@
-export function tempoExecucao(){
-    return function(
-        target: any,
-        propertyKey: string,
-        descriptor: PropertyDescriptor
-    ){
-        return descriptor
-    }
-}
